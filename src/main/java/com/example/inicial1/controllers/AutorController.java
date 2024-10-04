9302e034a644ee17cd3762e2965084f2136c018b
LucianoGolden <153865280+LucianoGolden@users.noreply.github.com>
2024-10-04 11:28:37 -0300

    ultimo commit

--- src/main/java/com/example/inicial1/controllers/AutorController.java
+++ src/main/java/com/example/inicial1/controllers/AutorController.java
@@ -3,9 +3,12 @@ package com.example.inicial1.controllers;
 
 import com.example.inicial1.entities.Autor;
 import com.example.inicial1.services.AutorServiceImpl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
 
 @RestController
 @CrossOrigin(origins = "*")
